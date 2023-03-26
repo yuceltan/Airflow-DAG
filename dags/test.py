096fe0e8addb2f31d202a818cf9cff56c610f01b
Yüceltan Ebiri <yuceltanebiri@outlook.com>
2023-03-26 04:46:43 +0200

    Exercise 2 Updated

--- dags/test.py
+++ dags/test.py
@@ -1,6 +1,16 @@
-import json
 import time
+import json
 import requests
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+from airflow.operators.python import BranchPythonOperator
+
+import pandas as pd
+import numpy as np
+import os
+
+
 def get_data():
     # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
     api_key = 'H9BZCXCEOBDGG625'
@@ -8,10 +18,27 @@ def get_data():
     r = requests.get(url)
     try:
         data = r.json()
-        path = r"C:\Users\yucel\Data_Center\Data Lake/"
-        with open(path + "stock_market_raw_data" + "IBM_" + str(time.time()), "w") as outfile:
-            json.dump(data, outfile)
-    except:
-       print("file could not be written")
 
-get_data()
+        with open("IBM_" + str(time.time())+".txt", "w") as outfile:
+            json.dump(data, outfile)
+            print("done!")
+    except:
+        pass
+
+def newPrint():
+    print("TEST!")
+
+
+default_dag_args = {
+    'start_date': datetime(2023, 2, 20),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'project_id': 1
+
+}
+
+with DAG("market_data_alphavantage_dag", schedule_interval='@daily', catchup=False, default_args=default_dag_args) as dag_python:
+    task_0 = PythonOperator(task_id='first_python_dag', python_callable=get_data)
+
